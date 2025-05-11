7008194b498ef89ff6cc48ce65dafb4365c8b5a7
Anton <132440944+lolipolovkovith@users.noreply.github.com>
2025-05-11 21:00:29 +0300

    Delete ST-5 directory

--- ST-5/src/main/java/com/andreychh/App.java
+++
@@ -1,9 +0,0 @@
-package com.andreychh;
-
-public class App {
-    public static void main(String[] args) {
-        double num = 2.0;
-        Sqrt sqrt = new Sqrt(num, 0.00000001);
-        System.out.println("Sqrt of " + num + " is " + sqrt.calc());
-    }
-}
